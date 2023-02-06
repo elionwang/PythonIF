76b11d483aa7e692ad77dd8f69ff4f749c42bb69
Elion Wang <74402660+elionwang@users.noreply.github.com>
2023-02-06 14:21:21 +0000

    time.sleep, prototype score

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
 import time
 import random
 
+matches = int(input('Hoeveel potjes wil je spelen? '))
 
-computerActie = random.randint(0, 2)
+i = 1
 
-if computerActie == 0:
-  computerActieNaam = 'steen'
-elif computerActie == 1:
-  computerActieNaam = 'papier'
-elif computerActie == 2:
-  computerActieNaam = 'schaar'
+spelerScore = 0
+computerScore = 0
 
-spelerInput = input('Kies steen[0], papier[1] of schaar[2]: ')
+while i <= matches:
 
-spelerActie = int(spelerInput)
+  computerActie = random.randint(0, 2)
 
-if spelerActie == 0:
-  spelerActieNaam = 'steen'
-elif spelerActie == 1:
-  spelerActieNaam = 'papier'
-elif spelerActie == 2:
-  spelerActieNaam = 'schaar'
-
-if spelerActie == computerActie:
-  print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Het is gelijkspel!')
-elif spelerActie == 0:
-  if computerActie == 1:
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint!')
-  elif computerActie == 2:
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint!')
-elif spelerActie == 1:
   if computerActie == 0:
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint!')
-  elif computerActie == 2:
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint!')
-elif spelerActie == 2:
-  if computerActie == 0:
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint!')
+    computerActieNaam = 'steen'
   elif computerActie == 1:
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint!')
+    computerActieNaam = 'papier'
+  elif computerActie == 2:
+    computerActieNaam = 'schaar'
+
+  spelerInput = input('Kies steen[0], papier[1] of schaar[2]: ')
+
+  time.sleep(3)
+
+  spelerActie = int(spelerInput)
+
+  if spelerActie == 0:
+    spelerActieNaam = 'steen'
+  elif spelerActie == 1:
+    spelerActieNaam = 'papier'
+  elif spelerActie == 2:
+    spelerActieNaam = 'schaar'
+
+  if spelerActie == computerActie:
+    spelerScore = spelerScore + 1
+    computerScore += 1
+    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Het is gelijkspel! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+  elif spelerActie == 0:
+    if computerActie == 1:
+      computerScore += 1
+      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+    elif computerActie == 2:
+      spelerScore += 1
+      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+  elif spelerActie == 1:
+    if computerActie == 0:
+      spelerScore += 1
+      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+    elif computerActie == 2:
+      computerScore += 1
+      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+  elif spelerActie == 2:
+    if computerActie == 0:
+      computerScore += 1
+      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+    elif computerActie == 1:
+      spelerScore += 1
+      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+
+  i += 1
