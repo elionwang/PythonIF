a7ae89ee81e848b4280dc47121fcb3f50cddef35
Elion Wang <74402660+elionwang@users.noreply.github.com>
2023-02-09 18:11:14 +0000

    Meer comments

--- 5. Steen papier schaar.py
+++ 5. Steen papier schaar.py
@@ -41,27 +41,27 @@ while i <= matches: # maak while loop, voer uit als i kleiner of gelijk is aan v
   if spelerActie == computerActie: # check of spelerActie hetzelfde is als computerActie en voer code uit als dit klopt
     spelerScore += 1 # voeg 1 toe aan variabele spelerScore
     computerScore += 1 # voeg 1 toe aan variabele computerScore
-    print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Het is gelijkspel!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}\033[0m |',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+    print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Het is gelijkspel!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}\033[0m |',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
   elif spelerActie == 0: # anders als spelerActie 0 is, voer code uit
     if computerActie == 1: # check of computerActie gelijk staat tot 1, en voer code uit als dit klopt
       computerScore += 1 # voeg 1 toe aan variabele computerScore
-      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}\033[0m |',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+      print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}\033[0m |',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
     elif computerActie == 2: # anders als computerActie 2 is, voer code uit
       spelerScore += 1 # voeg 1 toe aan variabele spelerScore
-      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+      print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
   elif spelerActie == 1: # check of spelerActie gelijk staat tot 1 en voer code uit als dit klopt
     if computerActie == 0: # check of computerActie gelijk staat tot 0, en voer code uit als dit klopt
       spelerScore += 1 # voeg 1 toe aan variabele spelerScore
-      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+      print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
     elif computerActie == 2: # anders als computerActie 2 is, voer code uit
       computerScore += 1 # voeg 1 toe aan variabele computerScore
-      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+      print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
   elif spelerActie == 2: # check of spelerActie gelijk staat tot 2, en voer code uit als dit klopt
     if computerActie == 0: # check of computerActie gelijk staat tot 0, en voer code uit als dit klopt
       computerScore += 1 # voeg 1 toe aan variabele computerScore
-      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+      print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
     elif computerActie == 1: # anders als computerActie 1 is, voer code uit
       spelerScore += 1 # voeg 1 toe aan variabele spelerScore
-      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
+      print(f'Pot {i}:\nJij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}',"\033[0m",'|',"\x1b[1;30;41m",f'Computer score: {computerScore}\033[0m') # geef uitslag en print naar console
 
   i += 1 # voeg 1 toe aan i, zodat while loop stopt na variabele 'matches' aantal aan potjes
