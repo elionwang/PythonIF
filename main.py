eb1a4f633233949c3ad0d09335bdec50cc31b85b
Elion Wang <74402660+elionwang@users.noreply.github.com>
2023-02-09 13:45:33 +0000

    Commit 09-02-2023, styling, fixes

--- main.py
+++ main.py
@@ -19,7 +19,12 @@ while i <= matches:
   elif computerActie == 2:
     computerActieNaam = 'schaar'
 
-  spelerInput = input('Kies steen[0], papier[1] of schaar[2]: ')
+  print('-------------------------------------')
+  spelerInput = int(input('Kies steen[0], papier[1] of schaar[2]: '))
+
+  if spelerInput < 0 or spelerInput > 2:
+    i -= 1
+    print(f'\x1b[4;30;41m{spelerInput} is geen geldige input idioot. Kies steen[0], papier[1] of schaar[2].',"\033[0m")
 
   time.sleep(3)
 
@@ -33,29 +38,29 @@ while i <= matches:
     spelerActieNaam = 'schaar'
 
   if spelerActie == computerActie:
-    spelerScore = spelerScore + 1
+    spelerScore += 1
     computerScore += 1
-    print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Het is gelijkspel! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+    print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;40m",'Het is gelijkspel!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
   elif spelerActie == 0:
     if computerActie == 1:
       computerScore += 1
-      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;41m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
     elif computerActie == 2:
       spelerScore += 1
-      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;42m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
   elif spelerActie == 1:
     if computerActie == 0:
       spelerScore += 1
-      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;42m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
     elif computerActie == 2:
       computerScore += 1
-      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;41m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
   elif spelerActie == 2:
     if computerActie == 0:
       computerScore += 1
-      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Computer wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;41m",'Computer wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
     elif computerActie == 1:
       spelerScore += 1
-      print(f'Jij koos: {spelerActieNaam} \n Computer koos: {computerActieNaam} \n Jij wint! \n Jouw score: {spelerScore} | Computer score: {computerScore}')
+      print('Jij koos:',"\x1b[3;37;40m",f'{spelerActieNaam}',"\033[0m",'\nComputer koos:',"\x1b[3;37;40m",f'{computerActieNaam}',"\n\x1b[1;30;42m",'Jij wint!',"\n\x1b[1;30;42m",f'Jouw score: {spelerScore}' "\033[0m",'|', "\x1b[1;30;41m",f'Computer score: {computerScore}',"\033[0m")
 
   i += 1
